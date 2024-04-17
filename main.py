b41af69243eca60d483ef996b8b1c4a6fbc11ba1
Anton Chen <contact@antonchen.ca>
2024-04-17 00:31:52 -0700

    add sentences to tree parsing function

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
 import json
-from parse_tree import parse_tree_string, print_tree
+from parse_tree import parse_trees, print_tree
 
-
-def read_jsonl_file(file_path):
-  trees = []
-  with open(file_path, 'r') as file:
-    for line in file:
-      json_data = json.loads(line)
-      trees.append(json_data)
-  return trees
-
-
-# Example usage:
 file_path = 'data/dataset/task_1/dev.jsonl'
-trees = read_jsonl_file(file_path)
+trees = parse_trees(file_path)
+
 for i in range(20):
-  lisp_proof = json.dumps(trees[i]["meta"]["lisp_proof"])[1:-1]
-  print(f'\n\nLISP PROOF {i}')
-  print(lisp_proof)
-  tree = parse_tree_string(lisp_proof)
-  print_tree(tree)
+  print_tree(trees[i])
--- parse_tree.py
+++ parse_tree.py
@@ -1,13 +1,38 @@
-class TreeNode:
+import json
 
-  def __init__(self, id):
+
+class TreeNode:
+  """
+  Data structure representing entailment tree.
+  """
+
+  def __init__(self, id, val):
     self.id = id
+    self.val = val
     self.children = []
 
 
-def parse_tree_string(tree_string):
+def parse_trees(file_path):
   """
-  Parses a lisp proof formatted string into a tree of type TreeNode.
+  Parses JSON encoded dateset stored at filepath and returns a list of trees.
+  """
+  trees_json = []
+  with open(file_path, 'r') as file:
+    for line in file:
+      json_data = json.loads(line)
+      trees_json.append(json_data)
+
+  trees = [parse_tree(tree_json) for tree_json in trees_json]
+
+  return trees
+
+
+def parse_tree(tree_json):
+  """
+  Parses a JSON encoded object from the EntailmentBank dataset
+  into a tree with id and value (sentence).
+
+  The tree structure is encoded in tree_json["meta"]["lisp_proof"]
 
   E.g. "((((sent1 sent3) -> int1) sent2) -> int2)"
 
@@ -23,18 +48,19 @@ def parse_tree_string(tree_string):
           └── sent3
   """
 
-  def parse_helper(tokens, start, end):
+  def parse_helper(tokens, id_to_sentence, start, end):
     """
     Must have a single root node.
 
     String looks like "((((sent1 sent3) -> int1) sent2) -> int2)"
     """
-    root = TreeNode(tokens[end - 1])
-    root.children = parse_children(tokens, start + 1, end - 3)
+    id = tokens[end - 1]
+    root = TreeNode(id, id_to_sentence[id])
+    root.children = parse_children(tokens, id_to_sentence, start + 1, end - 3)
 
     return root
 
-  def parse_children(tokens, start, end):
+  def parse_children(tokens, id_to_sentence, start, end):
     """
     Parses the children into an array.
     Implemented using a stack.
@@ -48,11 +74,12 @@ def parse_tree_string(tree_string):
     while i < end:
       # Case 1: base case (leaf node)
       if tokens[i] != "(":
-        children.append(TreeNode(tokens[i]))
+        id = tokens[i]
+        children.append(TreeNode(id, id_to_sentence[id]))
       # Case 2: recursive case
       else:
         j = find_matching_parenthesis(tokens, i)
-        children.append(parse_helper(tokens, i, j))
+        children.append(parse_helper(tokens, id_to_sentence, i, j))
         i = j
 
       i += 1
@@ -70,12 +97,28 @@ def parse_tree_string(tree_string):
           return i
     return -1  # No matching right parenthesis found
 
+  def parse_sentences(tree_json):
+    """
+    Extracts sentences from tree json from dataset.
+    Each sentences corresponds to an id.
+
+    Initial premises are stored in "triples", while
+    Intermediate premises are stored in "intermediate_conclusions"
+    """
+    return {
+        **tree_json["meta"]["triples"],
+        **tree_json["meta"]["intermediate_conclusions"],
+    }
+
+  id_to_sentence = parse_sentences(tree_json)
+
+  tree_string = tree_json["meta"]["lisp_proof"]
   tokens = tree_string.replace('(', ' ( ').replace(')', ' ) ').split()
-  return parse_helper(tokens, 0, len(tokens) - 1)
+  return parse_helper(tokens, id_to_sentence, 0, len(tokens) - 1)
 
 
 def print_tree(node, depth=0):
   if node is not None:
-    print("  " * depth + node.id)
+    print("  " * depth + node.id + ": " + node.val)
     for child in node.children:
       print_tree(child, depth + 1)
