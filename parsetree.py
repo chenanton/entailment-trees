d91c75d90487592902424b09a72d0d714c7d8573
Anton Chen <contact@antonchen.ca>
2024-04-20 20:49:10 -0700

    add function to reduce dimensionality of embedding

--- parsetree.py
+++ parsetree.py
@@ -1,5 +1,8 @@
 import json
 import pickle
+import torch
+import numpy as np
+from sklearn.decomposition import PCA
 
 from entailmenttree import EntailmentTree
 
@@ -19,11 +22,48 @@ def parse_trees(file_path):
   return trees
 
 
+def reduce_embeddings(trees, d_new):
+  """
+  Given a list of trees, 
+  reduce the dimension of all embeddings
+  from d to d' via PCA. 
+
+  :param trees: the list of trees
+  :param d_new: reduced dimensionality
+
+  :return trees: the updated list of trees
+  """
+
+  # Get N x d tensor of all embeddings
+  embeddings = []
+  for tree in trees:
+    for id, embedding in tree.id_to_embedding.items():
+      embeddings.append(embedding.unsqueeze(0))
+
+  embeddings = torch.cat(embeddings, dim=0)
+  print(f'embeddings.shape = {embeddings.shape}')
+
+  # Fit PCA
+  N, d = embeddings.shape
+  pca = PCA(n_components=d_new)
+  pca.fit(embeddings.numpy())  # Fit PCA on the data
+
+  # Update embeddings
+  for tree in trees:
+    for id, embedding in tree.id_to_embedding.items():
+      e = embedding.unsqueeze(0).numpy()
+      tree.id_to_embedding[id] = torch.tensor(pca.transform(e)).squeeze()
+
+  return trees
+
+
 if __name__ == "__main__":
   dataset = "dev"
   original_dataset_fp = f'data/task_1/{dataset}.jsonl'
   processed_dataset_fp = f'data/processed/{dataset}.pkl'
 
   trees = parse_trees(original_dataset_fp)
+  trees = reduce_embeddings(trees, 32)
+
   with open(processed_dataset_fp, 'wb') as file:
     pickle.dump(trees, file)
